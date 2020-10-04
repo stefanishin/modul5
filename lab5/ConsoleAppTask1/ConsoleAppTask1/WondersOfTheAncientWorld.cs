24884e8b720f24d294d97c1ab0eacedde50d9727
StefanishinMikita <stefanishin92@gmail.com>
2020-10-04 12:07:09 +0300

    add class seven WondersOfTheAncientWorld

---
+++ lab5/ConsoleAppTask1/ConsoleAppTask1/WondersOfTheAncientWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTask1
+{
+    public enum WondersOfTheAncientWorld
+    {
+     Pyramid_of_Cheops,
+     Hanging_Gardens_of_Babylon,
+     Temple_of_Artemis_at_Ephesus,
+     Zeus_statue_in_Olympia,
+     Mausoleum_at_Halicarnassus,
+     Colossus_of_Rhodes,
+     Lighthouse_of_Alexandria
+    }
+}
